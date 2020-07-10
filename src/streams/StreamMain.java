1dc129474aa89d69097bffd8451648d94a8cd321
jpalmat <jimmy.palma1990@gmail.com>
2020-07-09 19:36:45 -0500

    supplier and generate

--- src/streams/StreamMain.java
+++ src/streams/StreamMain.java
@@ -1,8 +1,11 @@
 package streams;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamMain {
 
@@ -13,11 +16,24 @@ public class StreamMain {
 
 		//pass string to List customizing the data
 		String test = "this is a string";
-		List<String> listString = Arrays.stream(test.split("")).filter(y -> !y.equals(" ")).map(x -> x.toUpperCase()).collect(Collectors.toList());
+		List<String> listString = Arrays.stream(test.split("")).filter(y -> !y.equals(" "))
+				.map(x -> x.toUpperCase()).collect(Collectors.toList());
 		System.out.println(listString);
 		
-		
-		
+		//build a stream
+		Stream<String> streamBuild = Stream.<String>builder().add("a").add("b").build();
+		streamBuild.forEach(x -> System.out.println(x));
+
+		//supplier, no parameter and return the element
+		Supplier<Stream> supplier = ()-> listString.stream();;
+		System.out.println(supplier.get().collect(Collectors.toList()));
+
+		//generate stream
+		Supplier<String> supplier1 = ()-> "a";
+		Stream<String> streamGenerate = Stream.generate(supplier1);
+		streamGenerate.limit(5).forEach(x -> System.out.println(x));
+
+
 	}
 
 }
