0fba2cc906418d1f7165e09461a944f0b537d059
jpalmat <jimmy.palma1990@gmail.com>
2020-07-09 22:26:53 -0500

    list to map

--- src/streams/StreamMain.java
+++ src/streams/StreamMain.java
@@ -3,11 +3,15 @@ package streams;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class StreamMain {
 
@@ -46,6 +50,31 @@ public class StreamMain {
 		IntStream intStream = IntStream.range(1, 6);
 		//intStream.forEach(x -> System.out.println(x));
 		System.out.println(intStream.boxed().collect(Collectors.toList()));
-	}
 
+		//reduce
+		IntStream intStream1 = IntStream.range(1, 6);
+		System.out.println(intStream1.reduce((a,b)-> a+b));
+
+		IntStream intStream2 = IntStream.range(1, 6);
+		BiFunction<Integer, Integer, Integer> biFunctionTest = (a,b)-> {
+
+			return a+b;
+		};
+		System.out.println(intStream2.reduce((a, b)->biFunctionTest.apply(a, b)));
+
+		List<Integer> list2 = Arrays.asList(5, 6, 7);
+		int res = list2.parallelStream().reduce(1, (s1, s2) -> {
+			System.out.println("BiFunction "+s1 + " * " + s2);
+			return s1 * s2;});
+		System.out.println(res);
+
+		//pass list to map
+		String string = "this is a is test a is";
+		List<String> listString1 = Arrays.stream(string.split(" ")).collect(Collectors.toList());
+		Map<String, Integer> collect = listString1.parallelStream().
+						collect(Collectors.toConcurrentMap(
+								w -> w, w -> 1, Integer::sum));
+
+		System.out.println(collect);
+	}
 }
