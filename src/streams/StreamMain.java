2d9ed1d32b3def5af4ce0dcf5dc2985c2f11e2a7
jpalmat <jimmy.palma1990@gmail.com>
2020-07-09 21:36:12 -0500

    function and biFunction

--- src/streams/StreamMain.java
+++ src/streams/StreamMain.java
@@ -3,8 +3,10 @@ package streams;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamMain {
@@ -16,7 +18,8 @@ public class StreamMain {
 
 		//pass string to List customizing the data
 		String test = "this is a string";
-		List<String> listString = Arrays.stream(test.split("")).filter(y -> !y.equals(" "))
+		Function<String, Boolean> functTest = (x) -> x.equals(" ");//function get one parameter and return one
+		List<String> listString = Arrays.stream(test.split("")).filter(y -> !functTest.apply(y))
 				.map(x -> x.toUpperCase()).collect(Collectors.toList());
 		System.out.println(listString);
 		
@@ -29,11 +32,20 @@ public class StreamMain {
 		System.out.println(supplier.get().collect(Collectors.toList()));
 
 		//generate stream
-		Supplier<String> supplier1 = ()-> "a";
+		Supplier<String> supplier1 = ()-> {
+			System.out.println("inside supplier");
+			return "a";
+		};
 		Stream<String> streamGenerate = Stream.generate(supplier1);
 		streamGenerate.limit(5).forEach(x -> System.out.println(x));
 
+		//iterate
+		Stream<Integer> streamIterate = Stream.iterate(10, n->n).limit(2);
+		streamIterate.forEach(x -> System.out.println(x));
 
+		IntStream intStream = IntStream.range(1, 6);
+		//intStream.forEach(x -> System.out.println(x));
+		System.out.println(intStream.boxed().collect(Collectors.toList()));
 	}
 
 }
