f4d39b4e6520492cb26f6a6b07f894d991bdca7e
jpalmat <jimmy.palma1990@gmail.com>
2020-07-27 16:35:41 -0500

    stack

---
+++ src/dataSstructure/StackClass.java
@@ -0,0 +1,14 @@
+package dataSstructure;
+
+import java.util.Stack;
+
+public class StackClass {
+    public static void main(String[] args) {
+        Stack s = new Stack();
+
+        s.push(2);
+        s.push(3);
+
+        System.out.println(s.pop());
+    }
+}
