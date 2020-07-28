b1f2af96c29fd5142ce896d431bf5e757e0d157c
jpalmat <jimmy.palma1990@gmail.com>
2020-07-28 13:09:33 -0500

    reverse String iterative and recursive

---
+++ src/string/ReverseString.java
@@ -0,0 +1,27 @@
+package string;
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+    }
+
+    public static String reverse(String source){
+        if(source == null || source.isEmpty()){
+            return source;
+        }
+        String reverse = "";
+        for(int i = source.length() -1; i>=0; i--){
+            reverse = reverse + source.charAt(i);
+        }
+
+        return reverse;
+    }
+
+    public static String reverseRecursive(String str)
+    {
+        if (str.isEmpty())
+            return str;
+        //Calling Function Recursively
+        return reverseRecursive(str.substring(1)) + str.charAt(0);
+    }
+}
