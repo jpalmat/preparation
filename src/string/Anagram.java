04415b5d16ac532c4047385c8109a8d777efd86d
jpalmat <jimmy.palma1990@gmail.com>
2020-07-28 13:25:59 -0500

    anagram

---
+++ src/string/Anagram.java
@@ -0,0 +1,33 @@
+package string;
+
+import java.util.Arrays;
+
+public class Anagram {
+    //quicksort O(nlog(n))
+    public static boolean iAnagram(String word, String anagram) {
+        char[] charFromWord = word.toCharArray();
+        char[] charFromAnagram = anagram.toCharArray();
+        Arrays.sort(charFromWord);
+        Arrays.sort(charFromAnagram);
+        return Arrays.equals(charFromWord, charFromAnagram);
+    }
+
+    //O(n)
+    public static boolean isAnagram(String word, String anagram) {
+        if (word.length() != anagram.length()) {
+            return false;
+        }
+        char[] chars = word.toCharArray();
+        for (char c : chars) {
+            int index = anagram.indexOf(c);
+            if (index != -1) {
+                anagram = anagram.substring(0, index) + anagram.substring(index + 1, anagram.length());
+            } else {
+                return false;
+            }
+        }
+        return anagram.isEmpty();
+    }
+
+
+}
