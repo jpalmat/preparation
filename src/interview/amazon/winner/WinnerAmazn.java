b968bf6559d816198452656aac17f874e41a4592
jpalmat <jimmy.palma1990@gmail.com>
2020-08-09 00:55:13 -0500

    Maryam code

--- src/interview/amazon/winner/WinnerAmazn.java
+++ src/interview/amazon/winner/WinnerAmazn.java
@@ -101,5 +101,37 @@ The customer is not a winner as the customer has added the fruits in order of gr
             }
         }
         return position[0]==codeListU.size() ? 1 : 0;
+
+        /*
+        //Maryam
+        List<String> newList=codeList.stream().flatMap(Collection::stream).collect(Collectors.toList());
+        int length=shoppingCart.size()-newList.size();
+        if(length<0){
+            return 0;
+        }
+        else if(length==0){
+            for(int i=0;i<newList.size();++i){
+                if(!newList.get(i).equals(shoppingCart.get(i)) && !newList.get(i).equals("anything") ){
+                    return 0;
+                }
+            }
+            return 1;
+        }
+        else {//different >0
+            boolean flag1=true,flag2=true;
+            List<String> shopingCart1= shoppingCart.subList(0,shoppingCart.size()-length-1);
+            List<String> shopingCart2= shoppingCart.subList(length,shoppingCart.size()-1);
+            for(int i=0;i<newList.size();++i){
+                if(!newList.get(i).equals(shopingCart1.get(i))){
+                    flag1=false;
+                }
+            }
+            for(int i=0;i<newList.size();++i){
+                if(!newList.get(i).equals(shopingCart2.get(i))){
+                    flag2=false;
+                }
+            }
+            return (flag1 || flag2) ? 1:0;
+         */
     }
 }
