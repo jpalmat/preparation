3f44a02895f9cb6d7c4addee61b6529c9f61fd20
Jimmy <jimmy.palma1990@gmail.com>
2020-09-22 22:38:33 -0500

    Length encoding

---
+++ src/ITCCoderpad/RunLengthEncoding.java
@@ -0,0 +1,26 @@
+package ITCCoderpad;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RunLengthEncoding {
+
+    public static void main(String[] args) {
+        System.out.println(printRLE("wwwwaaadexxxxxx"));
+    }
+    public static String printRLE(String str)
+    {
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        for(int i = 1; i < str.length(); i++){
+            if(str.charAt(i) == str.charAt(i-1)) {
+                count++;
+                if(i != str.length() - 1)
+                    continue;
+            }
+            sb.append(str.charAt(i-1)).append(count);
+            count = 1;
+        }
+        return sb.toString();
+    }
+}
