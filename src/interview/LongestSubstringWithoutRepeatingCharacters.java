9593456ea4e20f9c249cb7d0faa036609ae86b64
Jimmy <jimmy.palma1990@gmail.com>
2020-09-24 00:55:49 -0500

    LongestSubstringWithoutRepeatingCharacters

---
+++ src/interview/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,30 @@
+package interview;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        System.out.println(longestUniqueSubsttr("geeksforgeeks"));
+    }
+
+    static int longestUniqueSubsttr(String s)
+    {
+        Set<String> setString = new HashSet<>();
+        int max = 0;
+        for(int i = 0; i < s.length(); i++){
+            for(int j = i; j < s.length(); j++){
+                if(setString.contains(String.valueOf(s.charAt(j)))){
+                    max = setString.size()> max ? setString.size() : max;
+                    setString = new HashSet<>();
+                    break;
+                } else {
+                    setString.add(String.valueOf(s.charAt(j)));
+                }
+            }
+
+        }
+        return max;
+    }
+}
