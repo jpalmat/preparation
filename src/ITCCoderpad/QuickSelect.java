1f12b094769e12e908127b4cc75adebfe639787b
Jimmy <jimmy.palma1990@gmail.com>
2020-09-27 12:48:57 -0500

    quickselect

---
+++ src/ITCCoderpad/QuickSelect.java
@@ -0,0 +1,40 @@
+package ITCCoderpad;
+
+public class QuickSelect {
+    // Driver Code
+    public static void main(String[] args) {
+        int[] arr = {10, 4, 6};
+        int ksmall = 3;
+        System.out.println(kSmallest(arr, 0, arr.length - 1, ksmall-1));
+    }
+
+    public static int kSmallest(int[] arr, int low, int top, int ksamll) {
+        int pivot = partitionArray(arr, low, top);
+        if(pivot == ksamll)
+            return arr[pivot];
+        else if(pivot<ksamll){
+            return kSmallest(arr, pivot + 1, top, ksamll);
+        } else {
+            return kSmallest(arr, low, pivot-1, ksamll);
+        }
+
+    }
+
+    private static int partitionArray(int[] arr, int low, int top) {
+        int pivot = arr[top], index=low;
+
+        for(int i = low; i <= top; i++){
+            if(arr[i]< pivot){
+                int temp = arr[i];
+                arr[i] = arr[index];
+                arr[index] = temp;
+                index++;
+            }
+        }
+
+        int temp = arr[top];
+        arr[top] = arr[index];
+        arr[index]=temp;
+        return index;
+    }
+}
