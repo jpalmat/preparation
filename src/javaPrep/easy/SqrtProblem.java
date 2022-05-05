174046c1e41ae87f73986bf6baf7cef477ba5771
jpalmat <jimmy.palma1990@gmail.com>
2022-05-04 22:45:02 -0500

    sqrt problem

---
+++ src/javaPrep/easy/SqrtProblem.java
@@ -0,0 +1,28 @@
+package javaPrep.easy;
+
+public class SqrtProblem {
+
+    public static void main(String[] args) {
+        System.out.println(mySqrt(2147395599));
+    }
+    public static int mySqrt(int x) {
+        long start = 0;
+        long end = x;
+
+        while (start + 1 < end) {
+            long mid = start + (end - start) / 2;
+            if (mid * mid == x) {
+                return (int)mid;
+            } else if (mid * mid < x) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (end * end == x) {
+            return (int)end;
+        }
+        return (int)start;
+    }
+}
