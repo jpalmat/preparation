207abbe6b6bf9c19feefa055a7f87bb271861066
jpalmat <jimmy.palma1990@gmail.com>
2022-12-15 13:42:46 -0600

    merge sort

---
+++ src/javaPrep/sort/GenerateList.java
@@ -0,0 +1,20 @@
+package javaPrep.sort;
+
+import java.util.Random;
+
+public class GenerateList {
+    protected static int[] inputArray = new int[10];
+
+    static {
+        Random rand = new Random();
+        for (int i = 0; i < inputArray.length; i++) {
+            inputArray[i] = rand.nextInt(10000);
+        }
+    }
+
+    protected static void printArray(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
---
+++ src/javaPrep/sort/MergeSort.java
@@ -0,0 +1,66 @@
+package javaPrep.sort;
+
+public class MergeSort extends GenerateList {
+
+    public static void main(String[] args) {
+        printArray(inputArray);
+
+        System.out.println("after");
+        mergeSort(inputArray);
+        printArray(inputArray);
+    }
+
+    private static void mergeSort(int[] inputArray) {
+        int inputLength = inputArray.length;
+
+        if(inputLength < 2) {
+            return;
+        }
+
+        int midIndex = inputLength/2;
+        int[] leftHalf = new int[midIndex];
+        int[] rightHalf = new int[inputLength - midIndex];
+
+        for(int i = 0; i < midIndex; i++){
+            leftHalf[i] = inputArray[i];
+        }
+        for(int i = midIndex; i < inputLength; i++){
+            rightHalf[i - midIndex] = inputArray[i];
+        }
+
+        mergeSort(leftHalf);
+        mergeSort(rightHalf);
+
+        merge(inputArray, leftHalf, rightHalf);
+    }
+
+    private static void merge(int[] inputArray, int[] leftHalf, int[] rightHalf){
+        int leftLength = leftHalf.length;
+        int rightLength = rightHalf.length;
+
+        int i =0, j = 0, k = 0;
+
+        while (i < leftLength && j < rightLength) {
+            if(leftHalf[i] < rightHalf[j]) {
+                inputArray[k] = leftHalf[i];
+                i++;
+            } else {
+                inputArray[k] = rightHalf[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < leftLength) {
+            inputArray[k] = leftHalf[i];
+            i++;
+            k++;
+        }
+        while(j < rightLength) {
+            inputArray[k] = rightHalf[j];
+            j++;
+            k++;
+        }
+
+    }
+}
