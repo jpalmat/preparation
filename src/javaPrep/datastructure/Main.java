27ce658f63bcb9229293c3f861658a7675bb80ae
jpalmat <jimmy.palma1990@gmail.com>
2022-12-20 15:26:44 -0600

    mian method for hastmap and list

---
+++ src/javaPrep/datastructure/Main.java
@@ -0,0 +1,46 @@
+package javaPrep.datastructure;
+
+import javaPrep.datastructure.hashtable.HashTableByArray;
+import javaPrep.datastructure.list.ListByArray;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ListByArray<String> myList = new ListByArray<>();
+        HashTableByArray<String, String> hashMap = new HashTableByArray<>();
+
+        for (int i = 0; i < 1000000; i++) {
+            myList.add("" + i);
+            hashMap.add("" + i, "" + i);
+        }
+
+        ListByArray<String> elements = new ListByArray<>();
+        Random randomizer = new Random();
+        for (int i = 0; i < 1000; i++) {
+            elements.add("" + randomizer.nextInt(2000000));
+        }
+
+        long listSearchStartTime = System.nanoTime();
+        for (int i = 0; i < elements.size(); i++) {
+            myList.contains(elements.value(i));
+        }
+        long listSearchEndTime = System.nanoTime();
+
+        long hashMapSearchStartTime = System.nanoTime();
+        for (int i = 0; i < elements.size(); i++) {
+            hashMap.get(elements.value(i));
+        }
+        long hashMapSearchEndTime = System.nanoTime();
+
+
+        long listSearch = listSearchEndTime - listSearchStartTime;
+        System.out.println("List: the search took about " + listSearch / 1000000 + " milliseconds (" +
+                listSearch + " nanoseconds.)");
+
+        long hashMapSearch = hashMapSearchEndTime - hashMapSearchStartTime;
+        System.out.println("Hash map: the search took about " + hashMapSearch / 1000000 +
+                " milliseconds (" + hashMapSearch + " nanoseconds.)");
+    }
+}
