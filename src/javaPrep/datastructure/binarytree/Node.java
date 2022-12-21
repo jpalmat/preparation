dfb212dc54fb86dfbcd2c94064586714c1099ccc
jpalmat <jimmy.palma1990@gmail.com>
2022-12-21 11:40:59 -0600

    create binary tree

---
+++ src/javaPrep/datastructure/binarytree/Node.java
@@ -0,0 +1,13 @@
+package javaPrep.datastructure.binarytree;
+
+public class Node {
+    int value;
+    Node left;
+    Node right;
+
+    Node(int value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
